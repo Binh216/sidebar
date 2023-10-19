9ef606b4b3cd48afa1702231fc52af8eb4b2f79a
Binh216 <hoangvanbinh.gov@gmail.com>
2023-10-19 16:54:56 +0700

    completed jquery

--- src/extensions/sidebar/SidebarApplicationCustomizer.ts
+++ src/extensions/sidebar/SidebarApplicationCustomizer.ts
@@ -5,9 +5,14 @@ import {
 } from '@microsoft/sp-application-base';
 
 import styles from './AppCustomizer.module.scss';
-import { escape } from '@microsoft/sp-lodash-subset';
- import { FontAwesomeIcon } from '@fortawesome/free-solid-svg-icons';
+import { SPComponentLoader } from '@microsoft/sp-loader';
 
+import {
+  SPHttpClient,
+  SPHttpClientResponse 
+} from '@microsoft/sp-http';
+
+import * as $ from 'jquery';
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
  * it will be deserialized into the BaseExtension.properties object.
@@ -19,6 +24,20 @@ export interface ISidebarApplicationCustomizerProperties {
   
 }
 
+export interface IGetSpListItemsWebPartProps {
+  description: string;
+}
+export interface ISPLists {
+  value: ISPList[];
+}
+
+export interface ISPList {
+  Id:string;
+  Title: string;
+  ParentWebUrl: string;
+  ItemCount:number;
+}
+
 
 
 /** A Custom Action which can be run during execution of a Client Side Application */
@@ -27,45 +46,67 @@ export default class SidebarApplicationCustomizer
 
   private _topPlaceholder: PlaceholderContent | undefined;
   
+  constructor() {
+    super();
+    SPComponentLoader.loadCss('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css');
+  }
 
 
   public onInit(): Promise<void> {
     
-
+   
     this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
 
     const navElement = document.getElementById('spLeftNav') as HTMLElement;
-    navElement.setAttribute("width","18vw");
-    navElement.innerHTML  = `
-      <div class="${styles.menu}">
-        <p class="${styles.title}">Cây thư mục</p>
-        <ul>
+    const siteHeader = document.getElementById('spSiteHeader') as HTMLElement;
+    const appBar = document.getElementById('sp-appBar') as HTMLElement;
+    appBar.setAttribute('style','display:none');
+    siteHeader.setAttribute('style','display:none');
+    navElement.setAttribute("style","width:18vw");
+    const url = `/_api/Web/Lists?$filter=BaseTemplate eq 101`;
+    this._getListData(url)
+    .then((response) => {
+      let items = `<div class="${styles.menu}">
+                    <p class="${styles.title}">Quản lý tài liệu</p>
+                    <ul>`;
+      for (let index = 0; index < response.value.length; index++) {
+        const lib = response.value[index];
+        if (lib.Title != "Documents" && lib.Title != "Site Assets") {
+          items += `
           <li>
             <a href="#">
-              <i class="fa-solid fa-house"></i>
-              <span class="${styles.text}">Trang chủ</span>
+              <i class="fa-solid fa-landmark"></i>
+              <span>${lib.Title}</span>
+              <i class="arrow fa-solid fa-angle-down"></i>
             </a>
+            <ul class="${styles.sub_menu}">
+              <li>
+                <a href="#">
+                  <i class="fa-solid fa-folder-open"></i>
+                  <span>${lib.Title}</span>
+                  <i class="arrow fa-solid fa-angle-down"></i>
+                </a>
+              </li>
+            </ul>
           </li>
-          <li>
-            <a href="#">
-              <i class="fa-solid fa-house"></i>
-              <span class="${styles.text}">Trang chủ</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa-solid fa-house"></i>
-              <span class="${styles.text}">Trang chủ</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    `;
+        `;
+        }
+        
+      }
 
+      items +=`</ul></div>`;
+      navElement.innerHTML  =items ;
+      
+    })
+    .catch(err => console.log(err));
     return Promise.resolve();
   }
 
+  
+    
   private _renderPlaceHolders(): void {
+
+    
   console.log("HelloWorldApplicationCustomizer._renderPlaceHolders()");
   console.log(
     "Available placeholders: ",
@@ -88,18 +129,13 @@ export default class SidebarApplicationCustomizer
     }
 
     if (this.properties) {
-      let topString: string = this.properties.Top;
-      if (!topString) {
-        topString = "(Top property was not defined.)";
-      }
+     
 
       if (this._topPlaceholder.domElement) {
         this._topPlaceholder.domElement.innerHTML = `
         <div class="${styles.app}">
           <div class="${styles.top}">
-            <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i> ${escape(
-              topString
-            )}
+            
           </div>
         </div>
         <div class="${styles.modal}">
@@ -120,7 +156,26 @@ export default class SidebarApplicationCustomizer
       }
     }
   }
-}
+
+    $(document).on('click','li',function () {
+      $(this).siblings().removeClass('active');
+      $(this).toggleClass('active');
+      $(this).find('ul').slideToggle();
+    })
+  
+  } 
+
+private _getListData(url : string) : Promise<any>{
+   return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + url,SPHttpClient.configurations.v1)
+        .then((response: SPHttpClientResponse) => {
+            return response.json().then((responseJSON) => {
+              if (responseJSON !== null) {
+                return responseJSON;
+              }
+            }).catch(err => console.log(err));
+          
+        }).catch(err => console.log(err));
+  }
 
 private _onDispose(): void {
   console.log('[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
